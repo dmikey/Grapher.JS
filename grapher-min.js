85796c13e72f3002da1cf56b038083d8254a9a23
Derek Anderson <toxigenicpoem@gmail.com>
2011-05-10 15:25:19 -0500

    added random bar chart colors

--- grapher-min.js
+++ grapher-min.js
@@ -7,9 +7,11 @@ if(G){if(!o){m(mp,G);return false;}
 G.style.width=o.width;G.style.height=o.height;G.style.overflow="hidden";if(!d){m(mp,G);return false;}
 G.innerHTML="<div id='chart' style='margin-left:15px;'></div>";c=document.getElementById("chart");c.style.width=(rI(o.width)-30);c.style.height=(rI(o.height)-30);c.style.borderBottom=c.style.borderLeft="1px solid "+o.borderColor;gT=-((rI(o.height)-15)/2)
 gL=rI(o.width)/2
-for(i=0;i<=1;i++){G.innerHTML+='<div id="axis%" style="position:absolute;margin-top:'+(gT*i)+'px;margin-left:'+((gL)-(gL*i))+';">%</div>'.replace(/%/gi,A[i])}
+for(i=0;i<=1;i++){G.innerHTML+='<div id="axis%" style="position:absolute;margin-top:'+(gT*i)+'px;margin-left:'+((gL)-(gL*i))+'px;">%</div>'.replace(/%/gi,A[i])}
 count=0
 if(!o.type){m("need chart type",G);return false;}
-var p=10;for(var i in d){if(d.hasOwnProperty(i)){switch(o.type){case"bar":G.innerHTML+='<div id="d" style="height:'+d[i]+'px;background-color:red;width:20px;position:absolute;margin-top:-'+(d[i]+1)+'px;margin-left:'+(20*count+20+p*count)+'px;margin-right:'+p+'px;"></div>';G.innerHTML+='<div id="d" style="width:20px;position:absolute;margin-top:-'+(d[i]+20)+'px;margin-left:'+(20*count+20+p*count)+'px;margin-right:'+p+'px;">'+(d[i])+'</div>';break;case"line":break;}};count++}}}
+var p=10;for(var i in d){if(d.hasOwnProperty(i)){switch(o.type){case"bar":G.innerHTML+='<div id="d" style="height:'+d[i]+'px;background-color:'+color()+';width:20px;position:absolute;margin-top:-'+(d[i]+1)+'px;margin-left:'+(20*count+20+p*count)+'px;margin-right:'+p+'px;"></div>';G.innerHTML+='<div id="d" style="width:20px;position:absolute;margin-top:-'+(d[i]+20)+'px;margin-left:'+(20*count+20+p*count)+'px;margin-right:'+p+'px;">'+(d[i])+'</div>';break;case"line":break;}};count++}}}
 function m(t,g){g.innerHTML=t;}
+function color(){var C=["red","blue","green","yellow","purple"]
+return C[Math.floor(Math.random()*C.length)]}
 function rI(i){return parseInt(i);}}(grapher,document);
--- grapher.js
+++ grapher.js
@@ -28,7 +28,7 @@ if (typeof Object.create != 'function'){
 			};
 		var mp = "missing param",c
 		var G = document.getElementById(g);
-	
+		
 		var A = ["X","Y"]
 		if (G) {
 			if(!o)	{m(mp,G);return false;}
@@ -46,7 +46,7 @@ if (typeof Object.create != 'function'){
 			gT = -((rI(o.height)-15) / 2)
 			gL = rI(o.width) / 2
 			for (i=0;i<=1;i++){
-				G.innerHTML += '<div id="axis%" style="position:absolute;margin-top:' + (gT*i) + 'px;margin-left:' + ((gL)-(gL*i)) + ';">%</div>'.replace(/%/gi, A[i])
+				G.innerHTML += '<div id="axis%" style="position:absolute;margin-top:' + (gT*i) + 'px;margin-left:' + ((gL)-(gL*i)) + 'px;">%</div>'.replace(/%/gi, A[i])
 			}
 			//plot the data
 			count = 0
@@ -56,9 +56,8 @@ if (typeof Object.create != 'function'){
 					   if (d.hasOwnProperty(i)){
 							switch (o.type) {
 								case "bar": 
-									G.innerHTML += '<div id="d" style="height:' + d[i] + 'px;background-color:red;width:20px;position:absolute;margin-top:-' + (d[i]+1) +  'px;margin-left:'+ (20*count+20+p*count) +'px;margin-right:' + p + 'px;"></div>';
-									G.innerHTML += '<div id="d" style="width:20px;position:absolute;margin-top:-' + (d[i]+20) +  'px;margin-left:'+ (20*count+20+p*count) +'px;margin-right:' + p + 'px;">' + (d[i]) + '</div>';
-									
+									G.innerHTML += '<div id="d" style="height:' + d[i] + 'px;background-color:' + color() + ';width:20px;position:absolute;margin-top:-' + (d[i]+1) +  'px;margin-left:'+ (20*count+20+p*count) +'px;margin-right:' + p + 'px;"></div>';
+									G.innerHTML += '<div id="d" style="width:20px;position:absolute;margin-top:-' + (d[i]+20) +  'px;margin-left:'+ (20*count+20+p*count) +'px;margin-right:' + p + 'px;">' + (d[i]) + '</div>';								
 									break;
 								case "line": 
 									break;
@@ -73,6 +72,10 @@ if (typeof Object.create != 'function'){
   //often used calls, made smaller
   function m(t,g){
 	g.innerHTML  = t;
+  }
+  function color(){
+	var C = ["red","blue","green","yellow","purple"]
+	return C [Math.floor(Math.random()*C.length)]
   }
   function rI(i){
 	return parseInt(i);
