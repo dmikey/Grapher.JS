7322240d15e3e590ce00196c5d3be3cb2187bd05
Derek Anderson <toxigenicpoem@gmail.com>
2011-05-13 14:47:49 -0700

    Edited grapher-min.js via GitHub

--- grapher-min.js
+++ grapher-min.js
@@ -1,39 +1,7 @@
-if(typeof Object.create!='function'){Object.create=function(o){function F(){}
-F.prototype=o
-return new F}}!function(window){var grapher=window.grapher={}}(window);var hz="h";!function(grapher,document){grapher.bar=function(g,d,o){var mp="missing param",c
-var G=document.getElementById(g);mkLin(0,10,20,25,G);if(G){if(!o||!d){m(mp,G);return false;}
-m(axis('y',o),G);var ddW=33,ddH=330,sH='50%',sW='33px',om='left',lm1='top',lm2='left',mt='22px',gh='height'
-if(o.style==hz){var ddW=330,ddH=33,sH='33px',sW='50%',om='top',lm1='right',lm2='top',mt='0px',gh='width'}
-guid='d'+gid();h='<dl id="'+guid+'">';for(var i in d){if(d.hasOwnProperty(i)){s=d[i]
-if(s.length>1){s=s[0]}
-h+='<dd><span style="'+gh+':'+s+'%;"><b>'+s+'</b></span></dd>'
-if(d[i].length>1){for(b=1;b<d[i].length;b++){h+='<dd class="sub" ><span style="'+gh+':'+d[i][b]+'%;"><b>'+d[i][b]+'</b></span></dd> '}}};}
-m(h+'</dl>'+axis('x',o)+'<style>ul.xAxis{float:left;clear:left;display:inline;width:454px;margin:0 0 0 27px;padding:0} ul.yAxis{display:inline;float:left;margin:14px 0 0;padding:0} ul.xAxis li{float:left;list-style:none;width:33px;text-align:center} ul.yAxis li{list-style:none;height:33px;text-align:right;float:left;clear:left} dl#'+guid+',dl#'+guid+' dt,dl#'+guid+' dd{margin:0;padding:0} dl#'+guid+'{width:454px;height:360px;padding-left:11px;float:left} dl#'+guid+' dd{position:relative;display:block;float:left;width:'+ddW+'px;height:'+ddH+'px;margin-top:'+mt+';} dl#'+guid+' span{position:absolute;display:block;width:'+sW+';bottom:0;left:0;z-index:1;color:#555;text-decoration:none;height:'+sH+';background:#456} dl#'+guid+' span b{display:block;font-weight:700;font-style:normal;float:left;line-height:200%;color:#fff;position:absolute;'+lm1+':5px;'+lm2+':3px;text-align:right;width:23px} dl#'+guid+' .sub{margin-'+om+':-33px} dl#'+guid+' .sub span{background:#978}</style>',G);}}
-function axis(a,o){h="",b=a
-if(o.style==hz){if(a=="y"){a="x"}else{a="y"}}
-for(x=0;x<=o[a+"units"].length-1;x++){if(o.style==hz){h='<li>'+o[a+"units"][x]+'</li>'+h;}
-else{h+='<li>'+o[a+"units"][x]+'</li>';}}
-return'<ul class="'+b+'Axis">'+h+'</ul>';}}(grapher,document);function m(t,g){g.innerHTML+=t;}
-function rI(i){return parseInt(i);}
-function gid(){var S4=function(){return(((1+Math.random())*0x10000)|0).toString(16).substring(1);};return(S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());}
-function mD(x,y,w,h,g)
-{g.innerHTML+='<div style="position:absolute;'+'left:'+x+'px;'+'top:'+y+'px;'+'width:'+w+'px;'+'height:'+h+'px;'+'clip:rect(0,'+w+'px,'+h+'px,0);'+'background-color:#000;"><\/div>';}
-function mkLin(x1,y1,x2,y2,g)
-{if(x1>x2)
-{var _x2=x2;var _y2=y2;x2=x1;y2=y1;x1=_x2;y1=_y2;}
-var dx=x2-x1,dy=Math.abs(y2-y1),x=x1,y=y1,yIncr=(y1>y2)?-1:1;if(dx>=dy)
-{var pr=dy<<1,pru=pr-(dx<<1),p=pr-dx,ox=x;while((dx--)>0)
-{++x;if(p>0){mD(ox,y,x-ox,1,g);y+=yIncr;p+=pru;ox=x;}
-else p+=pr;}
-mD(ox,y,x2-ox+1,1,g);}
-else
-{var pr=dx<<1,pru=pr-(dy<<1),p=pr-dy,oy=y;if(y2<=y1)
-{while((dy--)>0)
-{if(p>0){mD(x++,y,1,oy-y+1,g);y+=yIncr;p+=pru;oy=y;}
-else{y+=yIncr;p+=pr;}}
-mD(x2,y2,1,oy-y2+1,g);}
-else
-{while((dy--)>0)
-{y+=yIncr;if(p>0){this.mD(x++,oy,1,y-oy,g);p+=pru;oy=y;}
-else p+=pr;}
-mD(x2,oy,1,y2-oy+1,g);}}}
+if(typeof Object.create!="function")Object.create=function(a){function d(){}d.prototype=a;return new d};(function(a){a.grapher={}})(window);var hz="h";
+(function(a,d){function g(a,c){h="";b=a;c.style==hz&&(a=a=="y"?"x":"y");for(x=0;x<=c[a+"units"].length-1;x++)c.style==hz?h="<li>"+c[a+"units"][x]+"</li>"+h:h+="<li>"+c[a+"units"][x]+"</li>";return'<ul class="'+b+'Axis">'+h+"</ul>"}a.bar=function(a,c,e){a=d.getElementById(a);mkLin(0,10,20,25,a);if(a){if(!e||!c)return m("missing param",a),!1;m(g("y",e),a);var k=33,f=330,n="50%",l="33px",o="left",i="top",r="left",t="22px",q="height";e.style==hz&&(k=330,f=33,n="33px",l="50%",o="top",i="right",r="top",
+t="0px",q="width");guid="d"+gid();h='<dl id="'+guid+'">';for(var p in c)if(c.hasOwnProperty(p)&&(s=c[p],s.length>1&&(s=s[0]),h+='<dd><span style="'+q+":"+s+'%;"><b>'+s+"</b></span></dd>",c[p].length>1))for(b=1;b<c[p].length;b++)h+='<dd class="sub" ><span style="'+q+":"+c[p][b]+'%;"><b>'+c[p][b]+"</b></span></dd> ";m(h+"</dl>"+g("x",e)+"<style>ul.xAxis{float:left;clear:left;display:inline;width:454px;margin:0 0 0 27px;padding:0} ul.yAxis{display:inline;float:left;margin:14px 0 0;padding:0} ul.xAxis li{float:left;list-style:none;width:33px;text-align:center} ul.yAxis li{list-style:none;height:33px;text-align:right;float:left;clear:left} dl#"+
+guid+",dl#"+guid+" dt,dl#"+guid+" dd{margin:0;padding:0} dl#"+guid+"{width:454px;height:360px;padding-left:11px;float:left} dl#"+guid+" dd{position:relative;display:block;float:left;width:"+k+"px;height:"+f+"px;margin-top:"+t+";} dl#"+guid+" span{position:absolute;display:block;width:"+l+";bottom:0;left:0;z-index:1;color:#555;text-decoration:none;height:"+n+";background:#456} dl#"+guid+" span b{display:block;font-weight:700;font-style:normal;float:left;line-height:200%;color:#fff;position:absolute;"+
+i+":5px;"+r+":3px;text-align:right;width:23px} dl#"+guid+" .sub{margin-"+o+":-33px} dl#"+guid+" .sub span{background:#978}</style>",a)}}})(grapher,document);function m(a,d){d.innerHTML+=a}function rI(a){return parseInt(a)}function gid(){var a=function(){return((1+Math.random())*65536|0).toString(16).substring(1)};return a()+a()+"-"+a()+"-"+a()+"-"+a()+"-"+a()+a()+a()}
+function mD(a,d,g,j,c){c.innerHTML+='<div style="position:absolute;left:'+a+"px;top:"+d+"px;width:"+g+"px;height:"+j+"px;clip:rect(0,"+g+"px,"+j+'px,0);background-color:#000;"></div>'}
+function mkLin(a,d,g,j,c){if(a>g)var e=g,k=j,g=a,j=d,a=e,d=k;var e=g-a,k=Math.abs(j-d),f=d,n=d>j?-1:1;if(e>=k){for(var l=k<<1,o=l-(e<<1),i=l-e,d=a;e-- >0;)++a,i>0?(mD(d,f,a-d,1,c),f+=n,i+=o,d=a):i+=l;mD(d,f,g-d+1,1,c)}else if(l=e<<1,o=l-(k<<1),i=l-k,e=f,j<=d){for(;k-- >0;)i>0?(mD(a++,f,1,e-f+1,c),f+=n,i+=o,e=f):(f+=n,i+=l);mD(g,j,1,e-j+1,c)}else{for(;k-- >0;)f+=n,i>0?(this.mD(a++,e,1,f-e,c),i+=o,e=f):i+=l;mD(g,e,1,j-e+1,c)}};
